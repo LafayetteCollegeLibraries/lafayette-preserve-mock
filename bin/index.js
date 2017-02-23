307380d0cc4a56b03b42ee47e3690a054a5161f5
Adam Malantonio <malantoa@lafayette.edu>
2017-02-23 17:06:48 -0500

    move usage to bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,21 @@ var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 var debug = require('debug')('lafayette-preserve-mock:cli')
 
+var usage = [
+  '',
+  'Usage: lafayette-preserve-mock [options]',
+  '',
+  'Starts mock LafayettePreserve server.',
+  '',
+  'Options:',
+  '    -h, --help   Usage',
+  '    -p, --port   Specify which port the server will listen on (default: 3000)',
+  '',
+].join('\n')
+
 if (argv.h || argv.help) {
-  fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
-  process.exit(0);
+  console.log(usage)
+  return
 }
 
 var port = argv.p || argv.port || process.env.PORT || 3000;
