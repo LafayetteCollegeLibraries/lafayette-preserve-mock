83412934e295e8bf6913ffad0b7a553e0987d812
Adam Malantonio <malantoa@lafayette.edu>
2017-02-23 15:51:35 -0500

    allow catalog.json to have trailing slash

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var debug = require('debug')('lafayette-preserve-mock:routes/')
 
 function sendFile (path, mimeType) {
   return function (req, res) {
@@ -25,7 +26,7 @@ router.get('/', function(req, res) {
     res.send(JSON.stringify({'name': 'lafayette-preserve-mock'}));
 });
 
-router.get('/catalog.json', sendJSON('catalog'));
+router.get('/catalog.json/?', sendJSON('catalog'));
 router.get('/concern/generic_works/postcard.json', sendJSON('generic_work_postcard'));
 router.get('/concern/generic_works/print.json', sendJSON('generic_work_print'));
 router.get('/downloads/print', sendImage('print'));
