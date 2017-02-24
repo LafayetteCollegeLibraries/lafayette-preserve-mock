b0078d93412b101da49a459e5057bfd0721d73c8
Adam Malantonio <malantoa@lafayette.edu>
2017-02-24 17:03:24 -0500

    use absolute location for filepaths

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-var fs = require('fs');
-var express = require('express');
-var router = express.Router();
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
+var router = express.Router()
 
 // dummy index route for now
 router.get('/', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({'name': 'lafayette-preserve-mock'}));
-});
+})
 
 router.get('/catalog.json/?', catalog)
 router.get('/concern/generic_works/:id.json', handleWork)
@@ -22,13 +23,17 @@ function sendJSONHeader (res) {
 
 function catalog (req, res) {
   sendJSONHeader(res)
-  fs.createReadStream('public/data/catalog.json').pipe(res)
+  fs.createReadStream(file('data/catalog.json')).pipe(res)
+}
+
+function file (location) {
+  return path.join(path.resolve(__dirname, '../public'), location)
 }
 
 function handleWork (req, res) {
   res.setHeader('Content-Type', 'application/json');
   var id = req.params.id
-  var filepath = 'public/data/' + id + '.json'
+  var filepath = file('data/' + id + '.json')
 
   fs.access(filepath, function (err) {
     sendJSONHeader(res)
@@ -45,5 +50,5 @@ function handleWork (req, res) {
 
 function handleImages (req, res) {
   res.setHeader('Content-Type', 'image/jpeg')
-  fs.createReadStream('public/img/postcard.jpg').pipe(res)
+  fs.createReadStream(file('img/postcard.jpg')).pipe(res)
 }
